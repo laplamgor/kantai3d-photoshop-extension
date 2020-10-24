e1fb3a69c2134d68a62f2f7aea254517b1a10677
laplamgor <laplamgor@gmail.com>
2020-10-24 23:04:04 +0800

    Add shift depth operations and refactor GLSL

--- js/main.js
+++ js/main.js
@@ -10,218 +10,10 @@
 
         var csInterface = new CSInterface();
 
-        PIXI.DepthPerspectiveFilter = new PIXI.Filter(
-                `
-            #ifdef GL_ES
-            precision highp float;
-            #endif
+        var vert = window.cep.fs.readFile(csInterface.getSystemPath(SystemPath.EXTENSION) + '/js/kantai3d.vert');
+        var frag = window.cep.fs.readFile(csInterface.getSystemPath(SystemPath.EXTENSION) + '/js/kantai3d.frag');
 
-            attribute vec2 aVertexPosition;
-            attribute vec2 aTextureCoord;
-            varying vec2 vTextureCoord;
-
-            uniform mat3 projectionMatrix;
-
-            void main(void)
-            {
-                vTextureCoord = aTextureCoord;
-                gl_Position = vec4((projectionMatrix * vec3(aVertexPosition, 1.0)).xy, 0.0, 1.0);
-            }
-            `, 
-                `
-            precision mediump float;
-            uniform vec2 offset;
-            uniform vec2 pan;
-            uniform float zoom;
-            uniform int displayMode;
-
-            uniform sampler2D uSampler;
-            uniform sampler2D displacementMap;
-
-            uniform float textureScale;
-            uniform vec2 textureSize;
-            uniform float textureWidth;
-            uniform float textureHeight;
-            uniform float frameWidth;
-            uniform float frameHeight;
-            uniform vec2 canvasSize;
-
-            uniform vec4 filterArea;
-            uniform vec4 filterClamp;
-
-
-            varying vec2 vTextureCoord;
-            varying vec4 vColor;
-            uniform vec4 dimensions;
-            uniform vec2 mapDimensions;
-            uniform float scale;
-            uniform float focus;
-
-
-            vec2 mapPan(vec2 coord)
-            {
-                return vec2((coord[0]  + pan[0] / textureWidth / textureScale) / zoom,
-                            (coord[1] + pan[1] / textureHeight / textureScale) / zoom);
-            }
-
-            vec2 mapCoord2(vec2 coord)
-            {
-                return vec2(coord[0] * frameWidth / textureWidth / textureScale,
-                            coord[1] * frameHeight / textureHeight / textureScale);
-            }
-
-
-            vec4 textureDiffuseNoBg(vec2 coord)
-            {
-                vec2 c = coord;
-                vec2 scale = textureSize * ( min(canvasSize[0]/textureSize[0], canvasSize[1]/textureSize[1]) );
-
-                c -= 0.5;                   // Normalize
-                c = c * canvasSize + pan;   // Convert to pixel count, where origin is the center
-                c /= scale;
-
-                c /= zoom;
-                c += 0.5;                   // Unnormalize
-
-
-                if (c[0] <= 0.0 || c[0] >= 1.0 || c[1] <= 0.0 || c[1] >= 1.0)
-                {
-                    return vec4(0.0);
-                } else {
-                    return texture2D(uSampler, c);
-                }
-            }
-
-
-            vec4 textureDiffuse(vec2 coord)
-            {
-                vec4 withoutBg = textureDiffuseNoBg(coord);
-
-                return withoutBg * withoutBg[3] + vec4(0.5, 0.5, 1.0, 1.0) * (1.0-withoutBg[3]);
-            }
-
-            vec4 textureDepth(vec2 coord)
-            {
-                vec2 c = coord;
-
-                vec2 scale = textureSize * ( min(canvasSize[0]/textureSize[0], canvasSize[1]/textureSize[1]) );
-
-                c -= 0.5;                   // Normalize
-                c = c * canvasSize + pan;   // Convert to pixel count, where origin is the center
-                c /= scale;
-
-                c /= zoom;
-                c += 0.5;                   // Unnormalize
-
-
-                // if (c[0] <= 0.0 || c[0] >= 1.0 || c[1] <= 0.0 || c[1] >= 1.0)
-                // {
-                //     return vec4(0.0, 0.0, 0.0, 0.0);
-                // }
-                return texture2D(displacementMap, c);
-            }
-
-            vec4 normal(vec2 coord)
-            {
-                vec2 lineW = vec2(0.5 / frameWidth, 0.0);
-                vec2 lineH = vec2(0.0, 0.5 / frameHeight);
-
-
-                float leftD = textureDepth(coord - lineW).r;
-                float rightD = textureDepth(coord + lineW).r;
-                float upD = textureDepth(coord - lineH).r;
-                float downD = textureDepth(coord + lineH).r;
-
-                if (textureDiffuseNoBg(coord)[3] < 1.0)
-                {
-                    return vec4(0.5,0.5,1.0,1.0);
-                }
-
-                return vec4(0.5, 0.5, 1.0, 1.0) + vec4(leftD - rightD, upD - downD, 0.0, 0.0) * 100.0 * zoom;
-            }
-
-            vec4 normalMixed(vec2 coord)
-            {
-                return textureDiffuse(coord)  - vec4(0.5, 0.5, 1.0, 1.0) + normal(coord);
-            }
-
-            const float compression = 1.0;
-            const float dmin = 0.0;
-            const float dmax = 1.0;
-
-            // sqrt(2)
-            #define MAXOFFSETLENGTH 1.41421356
-            // 10 * 1.1
-            #define MAXZOOM 11.0
-
-            #define MAXSTEPS 600.0
-
-
-            float fit = min(canvasSize[0]/textureSize[0], canvasSize[1]/textureSize[1]);
-            float steps = max(MAXSTEPS *length(offset *zoom*fit), 30.0);
-
-            void main(void)
-            {
-                vec2 scale2 = scale * vec2(textureHeight / frameWidth,
-                                           textureWidth / frameHeight )
-                              * vec2(1, -1);
-                mat2 baseVector =
-                    mat2(vec2((0.5 - focus) * (offset * zoom*fit) - (offset * zoom*fit) / 2.0) * scale2,
-                         vec2((0.5 - focus) * (offset * zoom*fit) + (offset * zoom*fit) / 2.0) * scale2);
-
-
-                vec2 pos = (vTextureCoord);
-                mat2 vector = baseVector;
-
-                float dstep = compression / (steps - 1.0);
-                vec2 vstep = (vector[1] - vector[0]) / vec2((steps - 1.0));
-
-                vec2 posSumLast = vec2(0.0);
-                vec2 posSum = vec2(0.0);
-
-                float weigth = 1.0;
-                float dpos;
-                float dposLast;
-
-                for (float i = 0.0; i < MAXSTEPS * MAXOFFSETLENGTH * MAXZOOM; ++i)
-                {
-                    vec2 vpos = pos + vector[1] - i * vstep;
-                    dpos = 1.0 - i * dstep;
-                    float depth = textureDepth(vpos).r;
-
-                    if (textureDiffuseNoBg(vpos)[3] == 0.0) {
-                        depth = 0.0;
-                    }
-
-                    depth = clamp(depth, dmin, dmax);
-
-                    if (dpos > depth)
-                    {
-                        posSumLast = vpos;
-                        dposLast = dpos;
-                    }
-                    else
-                    {
-                        posSum = vpos;
-                        weigth = (depth - dposLast) / dstep;
-                        break;
-                    }
-                };
-                vec2 coord = (posSum - posSumLast) * -clamp(weigth * 0.5 + 0.5, 0.0, 1.5) + posSum;
-                if (displayMode == 0)
-                {
-                    gl_FragColor = textureDiffuse(coord);
-                }
-                else if (displayMode == 1)
-                {
-                    gl_FragColor = normal(coord);
-                }
-                else
-                {
-                    gl_FragColor = normalMixed(coord);
-                }
-
-            }`);
+        PIXI.DepthPerspectiveFilter = new PIXI.Filter(vert.data, frag.data);
 
         PIXI.DepthPerspectiveFilter.apply = function (filterManager, input, output)
         {
@@ -250,7 +42,6 @@
                 height: 50
             }
             );
-
 
         var depthMapImage = new PIXI.Sprite.fromImage("");
 
@@ -411,7 +202,6 @@
 
         resize();
 
-
         var menuJson = `{
                             "menu": [
                                 {
@@ -493,6 +283,60 @@
                                     ]
                                 },
                                 {
+                                    "id": "shiftDepth",
+                                    "label": "Shift Depth",
+                                    "enabled": true,
+                                    "checkable": false,
+                                    "checked": false,
+                                     "menu": [
+                                        {
+                                            "id": "shiftDepth+25",
+                                            "label": "+25",
+                                            "enabled": true,
+                                            "checkable": false,
+                                            "checked": false
+                                        },
+                                        {
+                                            "id": "shiftDepth+5",
+                                            "label": "+5",
+                                            "enabled": true,
+                                            "checkable": false,
+                                            "checked": false
+                                        },
+                                        {
+                                            "id": "shiftDepth+1",
+                                            "label": "+1",
+                                            "enabled": true,
+                                            "checkable": false,
+                                            "checked": false
+                                        },
+                                        {
+                                            "label": "---"
+                                        },
+                                        {
+                                            "id": "shiftDepth-1",
+                                            "label": "-1",
+                                            "enabled": true,
+                                            "checkable": false,
+                                            "checked": false
+                                        },
+                                        {
+                                            "id": "shiftDepth-5",
+                                            "label": "-5",
+                                            "enabled": true,
+                                            "checkable": false,
+                                            "checked": false
+                                        },
+                                        {
+                                            "id": "shiftDepth-25",
+                                            "label": "-25",
+                                            "enabled": true,
+                                            "checkable": false,
+                                            "checked": false
+                                        }
+                                    ]
+                                },
+                                {
                                     "label": "---"
                                 },
                                 {
@@ -518,64 +362,89 @@
 
         function contextMenuCallback(menuID)
         {
-            if (menuID === "basicTextureMode" || menuID === "normalMapMode" || menuID === "mixedMode") {
+            if (menuID === "basicTextureMode" || menuID === "normalMapMode" || menuID === "mixedMode")
+            {
                 changeDisplayMode(menuID);
             }
-            if (menuID === "reposition") {
+            if (menuID === "reposition")
+            {
                 loadBaseImage()
                 window.displacementFilter.uniforms.pan = [0.0, 0.0];
                 window.displacementFilter.uniforms.offset = [0.0, 0.0];
                 window.displacementFilter.uniforms.zoom = 1.0;
             }
-            switch(menuID) {
-                case "scaleDepth*2":
-                    adjustAllLevels(2.0);
-                    break;
-                case "scaleDepth*1.3":
-                    adjustAllLevels(1.3);
-                    break;
-                case "scaleDepth*1.1":
-                    adjustAllLevels(1.1);
-                    break;
-                case "scaleDepth/1.1":
-                    adjustAllLevels(1.0/1.1);
-                    break;
-                case "scaleDepth/1.3":
-                    adjustAllLevels(1.0/1.3);
-                    break;
-                case "scaleDepth/2":
-                    adjustAllLevels(0.5);
-                    break;
-                case "about":
-                    cep.util.openURLInDefaultBrowser("https://github.com/laplamgor/kantai3d-photoshop-extension");
-                    break;
+            switch (menuID)
+            {
+            case "scaleDepth*2":
+                adjustAllLevels(2.0, 0.0);
+                break;
+            case "scaleDepth*1.3":
+                adjustAllLevels(1.3, 0.0);
+                break;
+            case "scaleDepth*1.1":
+                adjustAllLevels(1.1, 0.0);
+                break;
+            case "scaleDepth/1.1":
+                adjustAllLevels(1.0 / 1.1, 0.0);
+                break;
+            case "scaleDepth/1.3":
+                adjustAllLevels(1.0 / 1.3, 0.0);
+                break;
+            case "scaleDepth/2":
+                adjustAllLevels(0.5, 0.0);
+                break;
+
+            case "shiftDepth+25":
+                adjustAllLevels(1.0, 25.0);
+                break;
+            case "shiftDepth+5":
+                adjustAllLevels(1.0, 5.0);
+                break;
+            case "shiftDepth+1":
+                adjustAllLevels(1.0, 1.0);
+                break;
+            case "shiftDepth-25":
+                adjustAllLevels(1.0, -25.0);
+                break;
+            case "shiftDepth-5":
+                adjustAllLevels(1.0, -5.0);
+                break;
+            case "shiftDepth-1":
+                adjustAllLevels(1.0, -1.0);
+                break;
+            case "about":
+                cep.util.openURLInDefaultBrowser("https://github.com/laplamgor/kantai3d-photoshop-extension");
+                break;
             }
         }
 
-        function changeDisplayMode(mode){
+        function changeDisplayMode(mode)
+        {
             csInterface.updateContextMenuItem("basicTextureMode", true, false);
             csInterface.updateContextMenuItem("normalMapMode", true, false);
             csInterface.updateContextMenuItem("mixedMode", true, false);
             csInterface.updateContextMenuItem(mode, true, true);
 
-            switch(mode) {
-              case "basicTextureMode":
+            switch (mode)
+            {
+            case "basicTextureMode":
                 window.displacementFilter.uniforms.displayMode = 0;
                 break;
-              case "normalMapMode":
+            case "normalMapMode":
                 window.displacementFilter.uniforms.displayMode = 1;
                 break;
-              case "mixedMode":
+            case "mixedMode":
                 window.displacementFilter.uniforms.displayMode = 2;
             }
         }
 
-
         // Load the image as the default
-        function loadBaseImage() {
+        function loadBaseImage()
+        {
             csInterface.evalScript("app.activeDocument.fullName.fsName.replace(/\\\\/g, '/')", function (path)
             {
-                if (path.toLowerCase().indexOf("_depth.psd") === -1) {
+                if (path.toLowerCase().indexOf("_depth.psd") === -1)
+                {
                     return; // Only generate PNG depth map if matching file name format
                 }
                 var url = path.replace('_depth', '').replace('.psd', '.png') + "?_=" + (new Date().getTime());
@@ -640,7 +509,8 @@
         var currentDepthPngPath = "";
         function updatePreview()
         {
-            if (currentDepthPngPath.toLowerCase().indexOf("_depth.png") === -1) {
+            if (currentDepthPngPath.toLowerCase().indexOf("_depth.png") === -1)
+            {
                 return; // Only generate PNG depth map if matching file name format
             }
             var scriptSavePng = `
@@ -673,21 +543,31 @@
             );
         }
 
+        function adjustAllLevels(multiplier, addition)
+        {
+            var reduction = 0.0;
+            if (addition < 0)
+            {
+                reduction = -addition;
+                addition = 0.0;
+            }
 
-        function adjustAllLevels(multiplier) {
-            var fromStart = 0;
-            var fromEnd = 255;
-            var toStart = 0;
-            var toEnd = 255;
+            var fromStart = 0 + reduction;
+            var fromEnd = 255 - addition;
+            var toStart = 0 + addition;
+            var toEnd = 255 - reduction;
 
-            if (multiplier > 1.0) {
+            if (multiplier > 1.0)
+            {
                 var offset = Math.floor(127.0 / multiplier);
-                fromStart = 127 - offset;
-                fromEnd = 128 + offset;
-            } else {
+                fromStart = 127 - offset + reduction;
+                fromEnd = 128 + offset - addition;
+            }
+            else
+            {
                 var offset = Math.floor(127.0 * multiplier);
-                toStart = 127 - offset;
-                toEnd = 128 + offset;
+                toStart = 127 - offset + addition;
+                toEnd = 128 + offset - reduction;
             }
 
             var scriptScale = `
@@ -718,7 +598,8 @@
                 }
             `;
 
-            csInterface.evalScript(scriptScale, function (path) {});
+            csInterface.evalScript(scriptScale, function (path)  {}
+            );
         }
     }
     init();
